36613935aa9356a22f81d72ad1006569c6ab7730
Tasin <shahalam.tasin@gmail.com>
2020-09-14 16:05:41 +0600

    Associative work for lcd

--- Floor_Washer/Floor_Washer.ino
+++ Floor_Washer/Floor_Washer.ino
@@ -1,24 +1,27 @@
 /**
- * Setting up pin variables accordingly:
- * Arduino  5 -> PWM      -> Relay for Water Pump       -> int relayPin
- * Arduino  6 -> PWM      -> Value for Servo Motor      -> int servoPin
- * Arduino  7 -> PWM      -> Speed of Left Side Wheel   -> int enLeft
- * Arduino  8 -> PWM      -> Speed of Right Side Wheel  -> int enRight
- * Arduino  9 -> PWM      -> Speed of Brush motors      -> int enBrush
- * Arduino 22 -> Digital  -> Right Wheel In1            -> int rIn1
- * Arduino 23 -> Digital  -> Right Wheel In2            -> int rIn2
- * Arduino 24 -> Digital  -> Left Wheel In1             -> int lIn1
- * Arduino 25 -> Digital  -> Left Wheel In2             -> int lIn2
- * Arduino 26 -> Digital  -> Brush Motor In1            -> int bIn1
- * Arduino 27 -> Digital  -> Brush Motor In2            -> int bIn2
- * Arduino 29 -> Digital  -> Sonar Trigger Pin          -> int sTRI
- * Arduino 30 -> Digital  -> Sonar Echo Pin             -> int sECH
- * Arduino 12 -> Digital  -> Buzzer Pin                 -> int buzz
- */
+   Setting up pin variables accordingly:
+   Arduino  5 -> PWM      -> Relay for Water Pump       -> int relayPin
+   Arduino  6 -> PWM      -> Value for Servo Motor      -> int servoPin
+   Arduino  7 -> PWM      -> Speed of Left Side Wheel   -> int enLeft
+   Arduino  8 -> PWM      -> Speed of Right Side Wheel  -> int enRight
+   Arduino  9 -> PWM      -> Speed of Brush motors      -> int enBrush
+   Arduino 22 -> Digital  -> Right Wheel In1            -> int rIn1
+   Arduino 23 -> Digital  -> Right Wheel In2            -> int rIn2
+   Arduino 24 -> Digital  -> Left Wheel In1             -> int lIn1
+   Arduino 25 -> Digital  -> Left Wheel In2             -> int lIn2
+   Arduino 26 -> Digital  -> Brush Motor In1            -> int bIn1
+   Arduino 27 -> Digital  -> Brush Motor In2            -> int bIn2
+   Arduino 29 -> Digital  -> Sonar Trigger Pin          -> int sTRI
+   Arduino 30 -> Digital  -> Sonar Echo Pin             -> int sECH
+   Arduino 12 -> Digital  -> Buzzer Pin                 -> int buzz
+*/
 #include <SoftwareSerial.h>
 #include <LiquidCrystal.h>
 #include <Servo.h>
 
+int wheelstatus ;
+int brushstatus ;
+int buzzerstatus;
 int relayPin = 5;
 int servoPin = 6;
 int enLeft = 7;
@@ -34,18 +37,19 @@ int sTRI = 29;
 int sECH = 30;
 int buzz = 12;
 
+
 LiquidCrystal lcd(34, 35, 36, 37, 38, 39);
 SoftwareSerial terminal(10, 11);
 Servo servo;
-int servoVal=0, motSpeed = 0;
+int servoVal = 0, motSpeed = 0;
 char in;
-int flag=0;
+int flag = 0;
 void setup() {
   // put your setup code here, to run once:
   lcd.begin(20, 4);
   Serial.begin(9600);
   terminal.begin(9600);
-  
+
   servo.attach(servoPin);
   delay(20);
 
@@ -61,235 +65,178 @@ void setup() {
   pinMode(bIn2, OUTPUT);
   pinMode(sTRI, OUTPUT);
   pinMode(sECH, INPUT);
-  pinMode(buzz,OUTPUT);
+  pinMode(buzz, OUTPUT);
 
 
   terminal.println("Setup complete");
 
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   lcd.print("Wheel:");
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   lcd.print("Brush:");
-  lcd.setCursor(0,2);
+  lcd.setCursor(0, 2);
   lcd.print("Pump:");
-  lcd.setCursor(0,3);
+  lcd.setCursor(0, 3);
   lcd.print("Servo:");
-  lcd.setCursor(10,1);
+  lcd.setCursor(10, 1);
   lcd.println("Speed:");
-  
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  if(Serial.available()){
+  if (Serial.available()) {
     in = Serial.read();
     terminal.println(in);
-    
-    if(in == 'F')
+
+    if (in == 'F') {
       goForward();
-    else if(in == 'B')
+      wheelstatus = 1;
+    }
+    else if (in == 'B') {
       goBackward();
-    else if(in == 'L')
+      wheelstatus = 2;
+    }
+    else if (in == 'L') {
       goLeft();
-    else if(in == 'R')
+      wheelstatus = 3;
+    }
+    else if (in == 'R') {
       goRight();
-    else if(in == 'S')
+      wheelstatus = 4;
+    }
+    else if (in == 'S') {
       wheelStop();
-    else if(in == 'W')
+      wheelstatus = 0;
+    }
+    else if (in == '2') {
       brushStart();
-    else if(in == 'w')
+      brushstatus = 1;
+    }
+
+    else if (in == '3') {
       analogWrite(enBrush, 0);
-    else if(in == 'U')
-      flag =1;
-      //servo.write(servoVal++);
-    else if(in == 'u')
-      flag =2;
-     // servo.write(servoVal--);
-    else if(in == 'O')
-      flag =0;
-     // servo.write(servoVal--);
-    else if(in == 'V')
+      brushstatus = 0;
+    }
+
+    else if (in == '0')
+      flag = 1; //brush up
+
+    else if (in == '1')
+      flag = 2; //brush down
+
+    else if (in == 'O')
+      flag = 0; //brush stop
+    // servo.write(servoVal--);
+    else if (in == '4')
       digitalWrite(relayPin, HIGH);
-    else if(in == 'v')
+    else if (in == '5')
       digitalWrite(relayPin, LOW);
-    else if(in >= '1' && in <= '9')
-      motSpeed = 28 * (in-'0');
-    delay(40);
+
+
   }
-  
-  long duration, inches, cm;
-  
+
+  long duration, cm;
+
   digitalWrite(sTRI, LOW);
   delayMicroseconds(2);
-  
+
   digitalWrite(sTRI, HIGH);
   delayMicroseconds(10);
-  
+
   digitalWrite(sTRI, LOW);
 
-  
+
   duration = pulseIn(sECH, HIGH); // using pulsin function to determine total time
   cm = microsecondsToCentimeters(duration); // calling method
 
 
-  if(cm <=10){
+  if (cm <= 10) {
 
-      tone(buzz,100);
-      terminal.println("!Buzzing!");
+    tone(buzz, 100);
+    terminal.println("!Buzzing!");
+    buzzerstatus = 1;
   }
-  else{
-     noTone(buzz);
+  else {
+    noTone(buzz);
+    buzzerstatus = 0;
+  }
+
+  if (flag == 1) {
+    servo.write(servoVal++);
+    if (servoVal > 180) {
+      flag = 0;
     }
-  terminal.println(servoVal);
-  if(flag==1){
-   servo.write(servoVal++);
-   if(servoVal>180){
-    flag =0;
-   }
   }
-  else if(flag==2){
-     servo.write(servoVal--);
-     if(servoVal<=0){
-    flag =0;
-   }
+  else if (flag == 2) {
+    servo.write(servoVal--);
+    if (servoVal <= 0) {
+      flag = 0;
+    }
   }
   delay(100);
-  
-  /*
-  terminal.println("Loop Starts");
 
-  terminal.println("Relay Part");
-  digitalWrite(relayPin, HIGH);
-  delay(1000);
-  digitalWrite(relayPin, LOW);
 
-  terminal.println("Servo Part");
-  for(servoVal = 0; servoVal <= 200; ++servoVal){
-    servo.write(servoVal);
-    delay(10);
-  }
-  while(servoVal--){
-    servo.write(servoVal);
-    delay(5);
-  }
-
-  terminal.println("Wheel Part");
-  analogWrite(enRight, 250);
-  analogWrite(enLeft, 250);
-  
-  // Go Forward
-  terminal.println(" Go Forward"); 
-  digitalWrite(rIn1, HIGH);
-  digitalWrite(rIn2, LOW);
-  digitalWrite(lIn1, HIGH);
-  digitalWrite(lIn2, LOW);
-
-  delay(2000);
-
-  // Go Backward
-  terminal.println(" Go Backward"); 
-  digitalWrite(rIn1, LOW);
-  digitalWrite(rIn2, HIGH);
-  digitalWrite(lIn1, LOW);
-  digitalWrite(lIn2, HIGH);
-
-  delay(2000);
-
-  // Go Right
-  terminal.println(" Go Right"); 
-  digitalWrite(rIn1, HIGH);
-  digitalWrite(rIn2, LOW);
-
-  delay(2000);
-
-  // Go Left
-  terminal.println(" Go Left"); 
-  digitalWrite(lIn1, LOW);
-  digitalWrite(lIn2, HIGH);
-
-  delay(2000);
-
-  // Stop All Wheel
-  terminal.println(" Stop All Wheel"); 
-  analogWrite(enRight, 0);
-  analogWrite(enLeft, 0);
-
-  terminal.println("Brush Part");
-  // Spin Brush Motors
-  digitalWrite(bIn1, HIGH);
-  digitalWrite(bIn2, LOW);
-
-  analogWrite(enBrush, 100);
-  delay(2000);
-
-  // Stop Brush Motors
-  analogWrite(enBrush, 0);
-  delay(2000);
-  */
 }
 
-void goForward(){
+void goForward() {
   terminal.println(" Go Forward");
   analogWrite(enRight, 250);
-  analogWrite(enLeft, 250); 
+  analogWrite(enLeft, 250);
   digitalWrite(rIn1, LOW);
   digitalWrite(rIn2, HIGH);
   digitalWrite(lIn1, HIGH);
   digitalWrite(lIn2, LOW);
 }
 
-void goBackward(){
+void goBackward() {
   terminal.println(" Go Backward");
   analogWrite(enRight, 250);
-  analogWrite(enLeft, 250); 
+  analogWrite(enLeft, 250);
   digitalWrite(rIn1, HIGH);
   digitalWrite(rIn2, LOW);
   digitalWrite(lIn1, LOW);
   digitalWrite(lIn2, HIGH);
 }
 
-void goRight(){
+void goRight() {
   terminal.println(" Go Right");
-  analogWrite(enRight, 250);
-  analogWrite(enLeft, 250); 
+  analogWrite(enRight, 0);
+  analogWrite(enLeft, 250);
   digitalWrite(lIn1, HIGH);
   digitalWrite(lIn2, LOW);
-  digitalWrite(rIn1, HIGH);
-  digitalWrite(rIn2, LOW);
+
 }
 
-void goLeft(){
+void goLeft() {
   terminal.println(" Go Left");
   analogWrite(enRight, 250);
-  analogWrite(enLeft, 250); 
+  analogWrite(enLeft, 0);
   digitalWrite(rIn1, LOW);
   digitalWrite(rIn2, HIGH);
-  digitalWrite(lIn1, LOW);
-  digitalWrite(lIn2, HIGH);
+
 }
 
-void wheelStop(){
+void wheelStop() {
   // Stop All Wheel
-  terminal.println(" Stop All Wheel"); 
+  terminal.println(" Brush motor stop");
+  terminal.println(" Stop All Wheel");
   analogWrite(enRight, 0);
   analogWrite(enLeft, 0);
 }
 
-void brushStart(){
+void brushStart() {
   // Spin Brush Motors
+  terminal.println(" Brush motor start");
   digitalWrite(bIn1, HIGH);
   digitalWrite(bIn2, LOW);
 
   analogWrite(enBrush, 100);
 }
 
-long microsecondsToInches(long microseconds) // method to covert microsec to inches 
-{
- return microseconds / 74 / 2;
-}
 
 long microsecondsToCentimeters(long microseconds) // method to covert microsec to centimeters
 {
-   return microseconds / 29 / 2;
+  return microseconds / 29 / 2;
 }
